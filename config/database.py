d1e6f1c3fabf91fd0be99af6d46b5f679073ea85
Renato Lins <renato.lins.digital@outlook.com>
2024-04-08 00:32:06 -0300

    updates database path config

--- config/database.py
+++ config/database.py
@@ -1,18 +1,24 @@
 import os
+from flask import Flask
 from pathlib import Path
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
-db = SQLAlchemy()
+db: SQLAlchemy = SQLAlchemy()
 
 
-def configure_local_database(app):
-    db_path = os.getenv('LOCAL_DATABASE_PATH')
-    if not db_path:
+def configure_local_database(app: Flask):
+    db_path: str | None = os.getenv('LOCAL_DATABASE_PATH')
+
+    if db_path is None:
         raise RuntimeError("LOCAL_DATABASE_PATH is not set")
-    os.makedirs(db_path, exist_ok=True)
 
-    project_dir = Path(__file__).resolve().parent.parent
-    db_uri = f'sqlite:///{project_dir}/{db_path}/results.db'
+    # Convert to Path object and resolve to absolute path
+    db_path = Path(db_path).resolve()
+    # Create directory if it doesn't exist
+    db_path.mkdir(parents=True, exist_ok=True)
+
+    db_uri: str = f'sqlite:///{db_path}/results.db'  # Create database URI
+    # Configure SQLAlchemy database URI
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
